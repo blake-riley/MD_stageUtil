bcf7219c5bf99e54363ff4226bbb01a25dc921de
Peter Chandler <peter.chandler1@monash.edu>
2017-07-20 16:45:07 +1000

    Update _MD.MonARCH.{sysname}.run1.EQ000.sh Changed reservation to gpu_buckle

--- _init/c-scriptGen/_MD.MonARCH.{sysname}.run1.EQ000.sh
+++ _init/c-scriptGen/_MD.MonARCH.{sysname}.run1.EQ000.sh
@@ -17,7 +17,7 @@
 #SBATCH --gres=gpu:2
 
 #SBATCH --time=0-24:00:00
-#SBATCH --reservation=buckle_lab
+#SBATCH --reservation=gpu_buckle
 
 
 #==============================================================
